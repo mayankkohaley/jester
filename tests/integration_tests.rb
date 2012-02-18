129a9135be3c5973b5f3e789638f1d449a022e0d
Mayank Kohaley <mayank.kohaley@gmail.com>
2012-02-18 15:02:48 +0530

    Added some more integration tests to cover most of the scenarios

--- tests/integration_tests.rb
+++ tests/integration_tests.rb
@@ -15,5 +15,49 @@ describe "Jester" do
     server.tick
     mailer.messages.first.subject.must_equal("Sorry, didn't understand your request")
   end
+    
+  it "responds with successful messages after a story is stored" do
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "a {genre} story '{title}'")
   
+    server.tick
+    mailer.messages.first.subject.must_equal("Jester saved '{title}'")                  
+         
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "what stories do you know?")
+
+    server.tick
+    mailer.messages.first.subject.must_equal("All of Jester's stories")
+    
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "tell me something {genre}")
+    
+    server.tick
+    mailer.messages.first.subject.must_equal('{title}')
+    
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "tell me '{title}'")
+                           
+    server.tick
+    mailer.messages.first.subject.must_equal('{title}')                           
+    
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "tell something {genre} to {email}")
+    
+    server.tick
+  end
+  
+  it "respond with an error message when story is not stored in jester" do
+    mailer.deliver_message(:to => 'test@test.com',
+                           :subject => "tell me '{title}'")
+    server.tick
+    mailer.messages.first.subject.must_equal("Couldn't find '{title}'")
+  end
+  
+  after do
+    if File.exist?(server.settings.application.jester_db) 
+      File.unlink(server.settings.application.jester_db) 
+    end
+  end
+
 end
